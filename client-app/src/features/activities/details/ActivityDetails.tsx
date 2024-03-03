258b7ac8a28cc6cc9dd3a60628b84b77886e5226
Randy De Leon <randyjdeleon@gmail.com>
2024-03-02 20:01:01 -0600

    Implemented SignalR for adding comments to activitys

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,8 +1,8 @@
-import { Button, Card, Grid, Image } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import LoadingComponent from '../../../app/layout/LoadingComponents';
 import { observer } from 'mobx-react-lite';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import ActivityDetailedHeader from './ActivityDetailedHeader';
 import ActivityDetailedInfo from './ActivityDetailedInfo';
@@ -11,12 +11,13 @@ import ActivityDetailedSidebar from './ActivityDeatiledSidebar';
 
 export default observer(function ActivityDetails() {
 const {activityStore} = useStore();
-const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
+const {selectedActivity: activity, loadActivity, loadingInitial, clearSelectedActivity} = activityStore;
 const {id} = useParams();
 
 useEffect(() => {
     if(id) loadActivity(id);
-}, [id, loadActivity])
+    return () => clearSelectedActivity();
+}, [id, loadActivity, clearSelectedActivity])
 
 if(loadingInitial || !activity) return <LoadingComponent />
 
@@ -25,7 +26,7 @@ if(loadingInitial || !activity) return <LoadingComponent />
             <Grid.Column width={10}>
                 <ActivityDetailedHeader activity={activity} />
                 <ActivityDetailedInfo activity={activity} />
-                <ActivityDetailedChat />
+                <ActivityDetailedChat activityId={activity.id} />
             </Grid.Column>
             <Grid.Column width={6}>
                 <ActivityDetailedSidebar activity={activity} />
